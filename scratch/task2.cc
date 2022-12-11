d02832284aa0a3ad6d0d5a720a113f1da99d10d9
ErMrJack <jacopodellepiane@icloud.com>
2022-12-11 16:48:44 +0100

    fixed include e rimosso mywirelessanimation

--- scratch/task2.cc
+++ scratch/task2.cc
@@ -16,11 +16,9 @@
 
 #include "ns3/applications-module.h"
 #include "ns3/core-module.h"
-#include "ns3/csma-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/mobility-module.h"
 #include "ns3/network-module.h"
-#include "ns3/point-to-point-module.h"
 #include "ns3/ssid.h"
 #include "ns3/yans-wifi-helper.h"
 #include "ns3/netanim-module.h"
